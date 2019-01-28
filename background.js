b8ee5a67e5d47a9e8071bf6040f023be5172715a
Boring <boring@boring.wang>
2019-01-28 23:26:28 +0800

    Use `append()` instead of `appendChild()`

--- background.js
+++ background.js
@@ -3,7 +3,7 @@
 const b = typeof chrome !== 'undefined' ? chrome : browser;
 
 const textarea = document.createElement('textarea');
-document.body.appendChild(textarea);
+document.body.append(textarea);
 
 b.browserAction.onClicked.addListener((tab) => {
     textarea.value = `${tab.title}\n${tab.url}`;
